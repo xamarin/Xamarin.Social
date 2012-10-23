f48a949d511899d64fac13fb0a74c960ad5e939a
Frank A. Krueger <fak@praeclarum.org>
2012-10-23 16:21:12 -0700

    Added Apache 2 license to Xamarin.Auth

--- Xamarin.Auth/AssemblyInfo.cs
+++ Xamarin.Auth/AssemblyInfo.cs
@@ -1,3 +1,18 @@
+//
+//  Copyright 2012, Xamarin Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -5,11 +20,11 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Xamarin.Auth")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cross platform authentication library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Xamarin Inc.")]
+[assembly: AssemblyProduct("Xamarin.Auth")]
+[assembly: AssemblyCopyright("2012 Xamarin Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
