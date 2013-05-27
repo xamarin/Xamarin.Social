264e6c7460687ced60d73f7142b2aebaad68d5dd
Dan Abramov <dan.abramov@gmail.com>
2013-05-28 01:47:52 +0400

    Implement Twitter Reverse Auth

--- src/Xamarin.Social.iOS/Services/Twitter6Service.cs
+++ src/Xamarin.Social.iOS/Services/Twitter6Service.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using MonoTouch.Social;
 using MonoTouch.Accounts;
@@ -8,6 +11,9 @@ namespace Xamarin.Social.Services
 {
 	public class Twitter6Service : SocialService, ITwitterService
 	{
+		public string ConsumerKey { get; set; }
+		public string ConsumerSecret { get; set; }
+
 		public Twitter6Service ()
 			: base ("Twitter", "Twitter", SLServiceKind.Twitter, ACAccountType.Twitter)
 		{
@@ -15,7 +21,69 @@ namespace Xamarin.Social.Services
 
 		public override Task<string> GetOAuthTokenAsync (Account acc)
 		{
-			return Task.Factory.StartNew<string> (() => { throw new NotImplementedException (); });
+			var tcs = new TaskCompletionSource<string> ();
+
+			new ReverseAuthRequest (acc, ConsumerKey, ConsumerSecret)
+				.GetResponseAsync ()
+				.ContinueWith (t => {
+					if (t.IsCanceled) {
+						tcs.SetCanceled ();
+						return;
+					} else if (t.IsFaulted) {
+						tcs.SetException (t.Exception);
+						return;
+					}
+
+					var parameters = new Dictionary<string, string> {
+						{ "x_reverse_auth_target", ConsumerKey },
+						{ "x_reverse_auth_parameters", t.Result.GetResponseText () }
+					};
+
+					this.CreateRequest ("GET", new Uri ("https://api.twitter.com/oauth/access_token"), parameters, acc)
+						.GetResponseAsync ()
+						.ContinueWith (tokenTask => {
+
+						if (tokenTask.IsCanceled) {
+							tcs.SetCanceled ();
+						} else if (tokenTask.IsFaulted) {
+							tcs.SetException (tokenTask.Exception);
+						} else {
+							tcs.SetResult (tokenTask.Result.GetResponseText ());
+						}
+					});
+				});
+
+			return tcs.Task;
+		}
+
+		class ReverseAuthRequest : OAuth1Request {
+			private string consumerKey;
+			private string consumerSecret;
+
+			public ReverseAuthRequest (Account account, string consumerKey, string consumerSecret)
+				: base (
+					"GET",
+					new Uri ("https://api.twitter.com/oauth/request_token"),
+					new Dictionary<string, string> { { "x_auth_mode", "reverse_auth" } },
+					account
+				)
+			{
+				this.consumerKey = consumerKey;
+				this.consumerSecret = consumerSecret;
+			}
+
+			protected override string GetAuthorizationHeader ()
+			{
+				return OAuth1.GetAuthorizationHeader (
+					Method,
+					Url,
+					new Dictionary<string, string> (Parameters),
+					this.consumerKey,
+					this.consumerSecret,
+					string.Empty,
+					string.Empty
+				);
+			}
 		}
 	}
 }
